14a6c611e61cad652ad02e3fa237cbd8950c9a98
NyiteX <kitheus09@gmail.com>
2022-09-23 13:29:34 +0300

    4\7

--- Program.cs
+++ Program.cs
@@ -1,66 +1,11 @@
 ﻿using System.Security.Cryptography.X509Certificates;
 
-class Massive_1
+class MassiveBasic
 {
-    public Random rnd = new Random();
-    private int x, y, s;
-    private double[] A;
-    private double[,] B;
-
-    public Massive_1(int X, int Y, int S)
-    {       
-        x = X;
-        y = Y;
-        s = S;
-        A = new double[X];
-        B = new double[Y, S];
-        Rand();
-    }
-    public bool Prover(string str)
-    {
-        if (str.Length > 0)
-        {
-            if (str[0] == '-' || Char.IsDigit(str[0]))
-            {
-                for (int i = 1; i < str.Length; i++)
-                {
-                    if (!Char.IsDigit(str[i]))
-                        return false;
-                }
-            }
-            else
-                return false;
-            return true;
-        }
-        return false;
-    }
-    public void Vvod1D()
-    {
-        Console.WriteLine("Enter " + x + "elements.");
-        string str = "adsad";
-        
-        for (int i = 0; i < x; i++)
-        {
-            while (!Prover(str))
-            {
-                Console.Write((i + 1) + " element: ");
-                str = Console.ReadLine();
-                if (!Prover(str)) Console.WriteLine("Wrong input");
-                else A[i] = int.Parse(str);
-            }
-            str = "adada";
-        }
-    }
-    public void Rand()
-    {
-        for (int i = 0; i < y; i++)
-        {
-            for (int u = 0; u < s; u++)
-            {
-                B[i, u] = Math.Round(rnd.NextDouble() * 100, 2);  //2 symbols after ' , '
-            }           
-        }
-    }
+    protected Random rnd = new Random();
+    protected int x, y, s;
+    protected double[] A;
+    protected double[,] B;
     public void PrintMas1D()
     {
         Console.WriteLine("\t1D mas");
@@ -82,6 +27,123 @@ class Massive_1
             Console.WriteLine();
         }
         Console.WriteLine();
+    }
+    public bool ProverWithMinus(string str)
+    {
+        if (str.Length > 0)
+        {
+            if (str[0] == '-' || Char.IsDigit(str[0]))
+            {
+                for (int i = 1; i < str.Length; i++)
+                {
+                    if (!Char.IsDigit(str[i]))
+                        return false;
+                }
+            }
+            else
+                return false;
+            return true;
+        }
+        return false;
+    }
+}
+class Massive_1: MassiveBasic
+{
+    public Massive_1(int X, int Y, int S)
+    {       
+        x = X;
+        y = Y;
+        s = S;
+        A = new double[X];
+        B = new double[Y, S];
+        Rand();
+    }
+    public Massive_1(int Y, int S)
+    {
+        y = Y;
+        s = S;
+        B = new double[Y, S];
+    }
+    
+    public void Summ()
+    {
+        double min = B[0, 0], max = B[0, 0];
+        int iMin = 0, iMax = 0, kol = 0;
+        for (int i = 0; i < y; i++)
+        {
+            for (int u = 0; u < s; u++)
+            {
+                kol++;
+                if (min > B[i, u])
+                {
+                    min = B[i, u];
+                    iMin = kol;
+                }
+                if (max < B[i, u])
+                {
+                    max = B[i, u];
+                    iMax = kol;
+                }
+            }
+        }
+        if (iMin > iMax)
+        {
+            int te = iMin;
+            iMin = iMax;
+            iMax = te;
+        }
+        kol = 0;
+        Console.WriteLine("min = " + min + "\nmax = " + max);
+        bool f = false;
+        for (int i = 0; i < y; i++)
+        {
+            for (int u = 0; u < s; u++)
+            {
+                kol++;
+                if (kol == iMin) f = true;
+                if (kol > iMax) f = false;
+                if (f) Console.Write(B[i, u] + "\t");
+            }
+        }
+        Console.WriteLine();
+    }
+    public void Vvod1D()
+    {
+        Console.WriteLine("Enter " + x + "elements.");
+        string str = "adsad";
+        
+        for (int i = 0; i < x; i++)
+        {
+            while (!ProverWithMinus(str))
+            {
+                Console.Write((i + 1) + " element: ");
+                str = Console.ReadLine();
+                if (!ProverWithMinus(str)) Console.WriteLine("Wrong input");
+                else 
+                    A[i] = int.Parse(str);               
+            }
+            str = "asdkasd";
+        }
+    }
+    public void Rand()
+    {
+        for (int i = 0; i < y; i++)
+        {
+            for (int u = 0; u < s; u++)
+            {
+                B[i, u] = Math.Round(rnd.NextDouble() * 100, 2);  //2 symbols after ' , '
+            }           
+        }
+    }
+    public void Rand(int a, int b)
+    {
+        for (int i = 0; i < y; i++)
+        {
+            for (int u = 0; u < s; u++)
+            {
+                B[i, u] = rnd.Next(a, b);
+            }
+        }
     }
     public void Max_Min()
     {
@@ -113,110 +175,65 @@ class Massive_1
         Console.WriteLine("summ Chetnix 1D = " + summChet);
         Console.WriteLine("summ Ne Chetnix stolbcov 2D = " + summNeChet);
     }
-    
-}
-class Massive_2
-{
-    public Random rnd = new Random();
-    int x, y;
-    private double[,] C;
-    public Massive_2(int X, int Y)
+    public void Umnozh()
     {
-        x = X;
-        y = Y;
-        C = new double[X, Y];
-    }
-    public bool Prover(string str)
-    {
-        if (str.Length > 0)
+        string str = "adsad";
+        int num = 0;
+        while (!ProverWithMinus(str))
         {
-            if (str[0] == '-' || Char.IsDigit(str[0]))
-            {
-                for (int i = 1; i < str.Length; i++)
-                {
-                    if (!Char.IsDigit(str[i]))
-                        return false;
-                }
-            }
-            else
-                return false;
-            return true;
+            Console.WriteLine("Umnozhenie na(chislo): ");
+            str = Console.ReadLine();
+            if (!ProverWithMinus(str)) Console.WriteLine("Wrong input");
+            else 
+                num = int.Parse(str);
         }
-        return false;
-    }
-    public void Rand()
-    {
-        for (int i = 0; i < x; i++)
+        for (int i = 0; i < y; i++)
         {
-            for (int u = 0; u < y; u++)
+            for (int u = 0; u < s; u++)
             {
-                C[i, u] = rnd.Next(-100,100);
+                B[i, u] *= num;
             }
         }
     }
-    public void PrintMas2D()
+    public void MatrixMath()
     {
-        Console.WriteLine("\t2D mas");
-        for (int i = 0; i < x; i++)
+        double[,] B2 = new double[y, s];
+        PrintMas2D();
+        Console.WriteLine("Second 2D mas");
+        for (int i = 0; i < y; i++)
         {
-            for (int u = 0; u < y; u++)
+            for (int u = 0; u < s; u++)
             {
-                Console.Write(C[i, u] + "\t");
+                B2[i, u] = Math.Round(rnd.NextDouble() * 100, 2);
+                Console.Write(B2[i, u]+"\t");
             }
             Console.WriteLine();
         }
-        Console.WriteLine();
-    }
-    public void Summ()
-    {
-        double min = C[0, 0], max = C[0, 0];
-        int iMin = 0, iMax = 0,kol = 0;
-        for (int i = 0; i < x; i++)
+        Console.WriteLine("\nUmnozhenie massivov");
+        for (int i = 0; i < y; i++)
         {
-            for (int u = 0; u < y; u++)
+            for (int u = 0; u < s; u++)
             {
-                kol++;
-                if (min > C[i, u])
-                {
-                    min = C[i, u];
-                    iMin = kol;
-                }
-                if (max < C[i, u])
-                {
-                    max = C[i, u];
-                    iMax = kol;
-                }
+                Console.Write(Math.Round(B2[i, u] * B[i, u],2) +"\t");
             }
+            Console.WriteLine();
         }
-        if(iMin > iMax)
+        Console.WriteLine("\nSumma massivov");
+        for (int i = 0; i < y; i++)
         {
-            int te = iMin;
-            iMin = iMax;
-            iMax = te;
-        }
-        kol = 0;
-        Console.WriteLine("min = " + min + "\nmax = " + max);
-        bool f = false;
-        for (int i = 0; i < x; i++)
-        {
-            for (int u = 0; u < y; u++)
+            for (int u = 0; u < s; u++)
             {
-                kol++;
-                if (kol == iMin) f = true;
-                if (kol > iMax) f = false;
-                if (f) Console.Write(C[i, u] + "\t");
+                Console.Write(Math.Round(B2[i, u] + B[i, u]) + "\t");
             }
+            Console.WriteLine();
         }
-        Console.WriteLine();
     }
+    
 }
 class Programm
 {
     static void Main()
-    {
-        const int x = 5, y = 3, s = 4;
-        Massive_1 M = new Massive_1(x, y, s);
-
+    {        
         char vvod;
         do
         {
@@ -227,55 +244,81 @@ class Programm
             switch (vvod)
             {
                 case '1':
-                    Console.Clear();
-                    M.Vvod1D();
-                    M.PrintMas1D();
-                    M.PrintMas2D();
-                    M.Max_Min();
-                    Console.WriteLine("\nPress any key to continue.");
-                    Console.ReadKey();
-                    break;
+                    {
+                        const int D1x = 5;
+                        const int D2x = 3;
+                        const int D2y = 4;
+                        Massive_1 M = new Massive_1(D1x, D2x, D2y);
+                        Console.Clear();
+                        M.Vvod1D();
+                        M.PrintMas1D();
+                        M.PrintMas2D();
+                        M.Max_Min();
+                        Console.WriteLine("\nPress any key to continue.");
+                        Console.ReadKey();
+                        break;
+                    }
                 case '2':
-                    Console.Clear();
-                    Massive_2 M2 = new Massive_2(5, 5);
-                    M2.Rand();
-                    M2.PrintMas2D();
-                    M2.Summ();
-                    Console.WriteLine("\nPress any key to continue.");
-                    Console.ReadKey();
-                    break;
+                    {
+                        Console.Clear();
+                        Massive_1 M2 = new Massive_1(5, 5);
+                        M2.Rand(-100, 100);
+                        M2.PrintMas2D();
+                        M2.Summ();
+                        Console.WriteLine("\nPress any key to continue.");
+                        Console.ReadKey();
+                        break;
+                    }
                 case '3':
-                    Console.Clear();
-                    
+                    {
+                        Console.Clear();
+                        Console.WriteLine("Cesar =/");
 
-                    Console.WriteLine("\nPress any key to continue.");
-                    Console.ReadKey();
-                    break;
+                        Console.WriteLine("\nPress any key to continue.");
+                        Console.ReadKey();
+                        break;
+                    }
                 case '4':
-                    Console.Clear();
-                    
-
-                    Console.WriteLine("\nPress any key to continue.");
-                    Console.ReadKey();
-                    break;
+                    {
+                        Console.Clear();
+                        Massive_1 M2 = new Massive_1(5, 5);
+                        M2.Rand(-100, 100);
+                        M2.PrintMas2D();
+                        M2.Umnozh();
+                        M2.PrintMas2D();
+                        Console.WriteLine("\nNext part: Press any key to continue.");
+                        Console.ReadKey();
+                        Console.Clear();
+                        M2.Rand(-10, 10);
+                        M2.MatrixMath();
+                        Console.WriteLine("\nPress any key to continue.");
+                        Console.ReadKey();
+                        break;
+                    }
                 case '5':
-                    Console.Clear();
-                    Console.WriteLine("Kalendar ? opyat ? no thx...");
-                    Console.WriteLine("\nPress any key to continue.");
-                    Console.ReadKey();
-                    break;
+                    {
+                        Console.Clear();
+                        
+                        Console.WriteLine("\nPress any key to continue.");
+                        Console.ReadKey();
+                        break;
+                    }
                 case '6':
-                    Console.Clear();
-                    
-                    Console.WriteLine("\nPress any key to continue.");
-                    Console.ReadKey();
-                    break;
+                    {
+                        Console.Clear();
+
+                        Console.WriteLine("\nPress any key to continue.");
+                        Console.ReadKey();
+                        break;
+                    }
                 case '7':
-                    Console.Clear();
-                    
-                    Console.WriteLine("\nPress any key to continue.");
-                    Console.ReadKey();
-                    break;
+                    {
+                        Console.Clear();
+
+                        Console.WriteLine("\nPress any key to continue.");
+                        Console.ReadKey();
+                        break;
+                    }
             }
         } while (vvod != 27);
 
